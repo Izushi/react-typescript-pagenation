f4c2e1ac9d4187fe1ffb507feafb09ffe93672cb
Izushi <shoeshinesamurai@gmail.com>
2024-09-20 23:46:19 -0700

    set the type for albums

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Album from './type';
 
 function App() {
+  const [albums, setAlbums] = useState<Album[]>([]);
+
   useEffect(() => {
     const getAlbums = async () => {
       await fetch("https://jsonplaceholder.typicode.com/albums/1/photos").then(
