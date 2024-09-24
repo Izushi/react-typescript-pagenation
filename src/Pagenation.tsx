4f32efd309f4a25aae07c94de65d8718463a4811
Izushi <shoeshinesamurai@gmail.com>
2024-09-23 20:22:29 -0700

    create pagenation

--- src/Pagenation.tsx
+++ src/Pagenation.tsx
@@ -1,6 +1,9 @@
 // Removed the unused import statement
 import AlbumList from "./AlbumList";
+import Album from "./type";
 import "./Pagenation.css";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
 
 type Props = {
   albums: Album[];
@@ -8,9 +11,25 @@ type Props = {
 
 const Pagenation = (props: Props) => {
   const { albums } = props;
+
+  const itemsPerPage = 6;
+
+  const [itemsOffset, setItemsOffset] = useState(0);
+
+  const endOffset = itemsOffset + itemsPerPage;
+
+  const currentAlbums = albums.slice(itemsOffset, endOffset);
+  const pageCount = Math.ceil(albums.length / itemsPerPage);
+
+  const handlePageClick = (e: { selected: number }) => {
+    const newOffset = (e.selected * itemsPerPage) % albums.length;
+    setItemsOffset(newOffset);
+  }
+
   return (
     <div className="albumWrapper">
-      <AlbumList albums={albums} />
+      <AlbumList albums={albums} currentAlbums={currentAlbums} />
+      <ReactPaginate pageCount={pageCount} onPageChange={handlePageClick} />
     </div>
   );
 };
